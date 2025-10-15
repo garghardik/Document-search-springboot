e8d8c631b7a291dd262a8db7a16e32244cfd4c67
Hardik Garg <hardikgarg151@gmail.com>
2025-10-15 22:50:08 +0530

    initialize project

---
+++ document-search/src/test/java/net/javaguides/document_search/DocumentSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.document_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
